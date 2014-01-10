cb1a8f0bd0762355393416c737ef3f52959a82db
John C. Earls <yt.rabb@gmail.com>
2014-01-10 09:47:48 -0800

    bugfix

--- s3shell.py
+++ s3shell.py
@@ -26,7 +26,7 @@ class S3ShellPlugin(DefaultClusterSetup):
     def run_scripts(self, nodes, user):
         log.info("Running %s on all nodes.")
         _, script =  os.path.split(self.path)
-        cmd1 = 'aws --region=us-east-1 cp s3://%s/%s /home/%s/%s' %\
+        cmd1 = 'aws s3 --region=us-east-1 cp s3://%s/%s /home/%s/%s' %\
                 (self.bucket, self.path, user, script)
         log.info("Running %s on all nodes." % cmd1)
         for node in nodes:
